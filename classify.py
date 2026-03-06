c14719f02845f68a07170cf956e3f3a5fee59f81
freambs <mgwfream@gmail.com>
2026-03-06 03:26:21 -0300

    fix classify output

--- classify.py
+++ classify.py
@@ -1,16 +1,27 @@
 import json
-from collections import Counter
 
 with open("all_battles_clean.json", "r", encoding="utf-8") as f:
     battles = json.load(f)
 
-types_counter = Counter()
+organized = {
+    "Treino": [],
+    "Torneio": []
+}
 
 for battle in battles:
     battle_info = battle.get("battle", {})
-    battle_type = battle_info.get("type", "unknown")
-    types_counter[battle_type] += 1
+    battle_type = battle_info.get("type", "")
 
-print("Distribuição por tipo de partida:")
-for t, count in types_counter.items():
-    print(f"{t}: {count}")
+    if battle_type == "friendly":
+        organized["Treino"].append(battle)
+
+    elif battle_type == "tournament":
+        organized["Torneio"].append(battle)
+
+print("Treinos:", len(organized["Treino"]))
+print("Torneios:", len(organized["Torneio"]))
+
+with open("type_output.json", "w", encoding="utf-8") as f:
+    json.dump(organized, f, indent=2, ensure_ascii=False)
+
+print("Arquivo type_output.json salvo.")
